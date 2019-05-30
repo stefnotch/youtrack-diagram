3e6dc40a68b6b3d843c501a889de01553fc4aa1d
Stefnotch <brandmairstefan@gmail.com>
2019-05-30 15:57:23 +0200

    🧹 Update Dependencies, remove axios

--- quasar.conf.js
+++ quasar.conf.js
@@ -4,7 +4,7 @@ module.exports = function(ctx) {
   return {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
-    boot: ["axios"],
+    boot: [],
 
     css: ["app.styl"],
 
